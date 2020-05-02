8ed0cb0bb1476cbe4f0a142abb20041ca2be8c7f
hellzkustom <hellzkustom@gmail.com>
2020-05-02 09:40:38 +0000

    投稿フォーム作成

---
+++ app/Http/Controllers/AdminBlogController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminBlogController extends Controller
+{
+    public function form()
+    {
+
+         return view('admin_blog.form');   
+
+    }
+}
---
+++ resources/views/admin_blog/form.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ブログ記事投稿フォーム</title>
+    {{--asset ヘルパー関数を使うと public/ 配下ファイルへのURLを生成してくれる--}}
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">   
+    <link rel="stylesheet" href="{{ asset('/public/css/blog.css') }}">
+</head>
+
+<body>
+    
+<h2>ブログ記事投稿・編集</h2>
+
+<form method="POST">
+    日付<br>
+    <input name="post_date" size="20" value="" placeholder="日付を入力して下さい。"><br><br>
+
+    タイトル<br>
+    <input name="title" value="" placeholder="タイトルを入力して下さい。"><br><br>
+
+    本文<br>
+    <textarea cols="50" rows="15" name="body" placeholder="本文を入力してください。"></textarea><br>
+
+    <input type="submit" value="送信">
+    {{--CSRFトークンが生成される--}}
+    {{ csrf_field() }}
+</form>
+    <!-- jqueryの読み込み -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- BootstrapのJS読み込み -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+</body>
+</html>
